61342be7dbf6575be5f0db7ed0b1abbb051764f4
Jeanie <ejung@pivotal.io>
2015-06-19 10:43:28 -0700

    Added three commands under GetMetadata: wildcard-apps, wildcard-delete-a, wildcard-delete-i

--- scaleover_plugin_withnotes.go
+++ scaleover_plugin_withnotes.go
@@ -1,5 +1,11 @@
 package main
+//scalable => Roll http traffic from one application to another
+//
+// NAME:
+//    scaleover - Roll http traffic from one application to another
 
+// USAGE:
+//    cf scaleover APP1 APP2 ROLLOVER_DURATION [--no-route-check]
 import (
 	"errors"
 	"fmt"
@@ -7,18 +13,23 @@ import (
 	"strconv"
 	"strings"
 	"time"
-	
+
+	//"github.com/andrew-d/go-termutil"
 	"github.com/cloudfoundry/cli/plugin"
 
-	"path/filepath"
+	"github.com/JustinTulloss/gogetter/wildcard"
+	"github.com/nilium/glob"
 	"github.com/cloudfoundry/cli/cf/terminal"
 )
 
 //Wildcard is this plugin
-type Wildcard struct {
-	matchedApps 	[]Apps
+type Wildcard struct { //struct == class
+	//app1     AppStatus
+	//app2     AppStatus
+	//maxcount 	int
+	filename 	string
+	filenameLength int
 }
-
 
 
 //GetMetadata returns metatada
@@ -38,23 +49,7 @@ func (cmd *Wildcard) GetMetadata() plugin.PluginMetadata {
 				UsageDetails: plugin.Usage{
 					Usage: "cf wildcard-apps APP_NAME_WITH_WILDCARD",
 				},
-			}, 
-			{
-				Name:     "wildcard-delete-a",
-				Alias:	  "wc-da"
-				HelpText: "Delete all apps in the target space that matches the wildcard",
-				UsageDetails: plugin.Usage{
-					Usage: "cf wildcard-delete-a APP_NAME_WITH_WILDCARD",
-				},
-			},
-			{
-				Name:     "wildcard-delete-i",
-				Alias:	  "wc-di"
-				HelpText: "Interactively delete apps in the target space that matches the wildcard",
-				UsageDetails: plugin.Usage{
-					Usage: "cf wildcard-delete-i APP_NAME_WITH_WILDCARD",
-				},
-			},//one argument
+			}, //one argument
 			// {
 			// 	Name:     "scaleover",
 			// 	HelpText: "Roll http traffic from one application to another",
@@ -74,41 +69,52 @@ func main() { //like Java's main
 //cmd is a pointer to an object of type DeleteWildCmb. 
 func (cmd *Wildcard) usage(args []string) error {
 	//counts number of args to confirm.
-	badArgs := 3 != len(args) //either 4 args
+	badArgs := 4 != len(args) //either 4 args
+
+	if 5 == len(args) {
+		if "--no-route-check" == args[4] {
+			badArgs = false
+		}
+	}
 
 	if badArgs {
-		return errors.New("Usage: cf wildcard-apps\n\tcf wildcard-apps APP_NAME_WITH_WILDCARD")
+		return errors.New("Usage: cf scaleover\n\tcf scaleover APP1 APP2 ROLLOVER_DURATION [--no-route-check]")
 	}
 	return nil
 }
 
+// func (cmd *Wildcard) shouldEnforceRoutes(args []string) bool { //totally specific to scaleover
+// 	return "--no-route-check" != args[len(args)-1]
+// }
+
+// func (cmd *Wildcard) parseTime(duration string) (time.Duration, error) { //totally specific to scaleover
+// 	rolloverTime := time.Duration(0)
+// 	var err error
+// 	rolloverTime, err = time.ParseDuration(duration)
+
+// 	if err != nil {
+// 		return rolloverTime, err
+// 	}
+// 	if 0 > rolloverTime {
+// 		return rolloverTime, errors.New("Duration must be a positive number in the format of 1m")
+// 	}
+
+// 	return rolloverTime, nil
+// }
 
 //Run runs the plugin
 //called everytime user executes the command
 func (cmd *Wildcard) Run(cliConnection plugin.CliConnection, args []string) {
 	if args[0] == "wildcard-apps" { //checking is very imp.
-		cmd.WildcardCommandApps(cliConnection, args)
-	} else if args[0] == "wildcard-delete-a" {
-		cmd.WildcardCommandDeleteAll(cliConnection, args)
-	} else if args[0] == "wildcard-delete-i" {
-		cmd.WildcardCommandDeleteInteractive(cliConnection, args)
+		cmd.ScaleoverCommand(cliConnection, args)
 	}
 }
-//Q: what is the error for?
-func (cmd *Wildcard) getAppInformation(cliConnection plugin.CliConnection, name string) (AppStatus, error) {
-	status := AppStatus {
-		name:           	name,
-		requested-state:   	0,
-		instances: 			string,
-		memory:          	string,
-		disk:        		string,
-		urls:				[]string{},
-	}
-}
-//WildcardCommand creates a new instance of this plugin
+
+//ScaleoverCommand creates a new instance of this plugin
 //this is the actual implementation
 //one method per command
-func (cmd *Wildcard) WildcardCommandApps(cliConnection plugin.CliConnection, args []string) {
+func (cmd *Wildcard) ScaleoverCommand(cliConnection plugin.CliConnection, args []string) {
+	enforceRoutes := cmd.shouldEnforceRoutes(args)
 
 	if err := cmd.usage(args); nil != err { //usage is just confirmation for correct number of args
 		fmt.Println(err) //printing
@@ -132,6 +138,14 @@ func (cmd *Wildcard) WildcardCommandApps(cliConnection plugin.CliConnection, arg
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	if enforceRoutes {
+		if err = cmd.errorIfNoSharedRoute(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
 	cmd.showStatus()
 
 	count := cmd.app1.countRequested
@@ -153,44 +167,44 @@ func (cmd *Wildcard) WildcardCommandApps(cliConnection plugin.CliConnection, arg
 	fmt.Println()
 }
 
-// func (cmd *Wildcard) getAppStatus(cliConnection plugin.CliConnection, name string) (AppStatus, error) {
-// 	status := AppStatus{
-// 		name:           name,
-// 		countRunning:   0,
-// 		countRequested: 0,
-// 		state:          "unknown",
-// 		routes:         []string{},
-// 	}
+func (cmd *Wildcard) getAppStatus(cliConnection plugin.CliConnection, name string) (AppStatus, error) {
+	status := AppStatus{
+		name:           name,
+		countRunning:   0,
+		countRequested: 0,
+		state:          "unknown",
+		routes:         []string{},
+	}
 
-// 	output, _ := cliConnection.CliCommandWithoutTerminalOutput("app", name)
+	output, _ := cliConnection.CliCommandWithoutTerminalOutput("app", name)
 
-// 	for idx, v := range output {
-// 		v = strings.TrimSpace(v)
-// 		if strings.HasPrefix(v, "FAILED") {
-// 			e := output[idx+1]
-// 			return status, errors.New(e)
-// 		}
-// 		if strings.HasPrefix(v, "requested state: ") {
-// 			status.state = strings.TrimPrefix(v, "requested state: ")
-// 		}
-// 		if strings.HasPrefix(v, "instances: ") {
-// 			instances := strings.TrimPrefix(v, "instances: ")
-// 			split := strings.Split(instances, "/")
-// 			status.countRunning, _ = strconv.Atoi(split[0])
-// 			status.countRequested, _ = strconv.Atoi(split[1])
-// 		}
-// 		if strings.HasPrefix(v, "urls: ") {
-// 			urls := strings.TrimPrefix(v, "urls: ")
-// 			status.routes = strings.Split(urls, ", ")
-// 		}
-// 	}
-// 	// Compensate for some CF weirdness that leaves the requested instances non-zero
-// 	// even though the app is stopped
-// 	if "stopped" == status.state {
-// 		status.countRequested = 0
-// 	}
-// 	return status, nil
-// }
+	for idx, v := range output {
+		v = strings.TrimSpace(v)
+		if strings.HasPrefix(v, "FAILED") {
+			e := output[idx+1]
+			return status, errors.New(e)
+		}
+		if strings.HasPrefix(v, "requested state: ") {
+			status.state = strings.TrimPrefix(v, "requested state: ")
+		}
+		if strings.HasPrefix(v, "instances: ") {
+			instances := strings.TrimPrefix(v, "instances: ")
+			split := strings.Split(instances, "/")
+			status.countRunning, _ = strconv.Atoi(split[0])
+			status.countRequested, _ = strconv.Atoi(split[1])
+		}
+		if strings.HasPrefix(v, "urls: ") {
+			urls := strings.TrimPrefix(v, "urls: ")
+			status.routes = strings.Split(urls, ", ")
+		}
+	}
+	// Compensate for some CF weirdness that leaves the requested instances non-zero
+	// even though the app is stopped
+	if "stopped" == status.state {
+		status.countRequested = 0
+	}
+	return status, nil
+}
 //HELPFUL
 // func (app *AppStatus) scaleUp(cliConnection plugin.CliConnection) { tot. specific
 // 	// If not already started, start it
