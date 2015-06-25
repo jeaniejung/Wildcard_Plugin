20a75286e87d5f5b40259e9920b6eb463ca9dbf3
Jeanie <ejung@pivotal.io>
2015-06-24 17:16:03 -0700

    wildcard_plugin_test added. Not building

---
+++ wildcard_plugin_suite_test.go
@@ -0,0 +1,18 @@
+//'go test' = 'gingko'
+package main
+
+import (
+	"github.com/cloudfoundry/cli/plugin/fakes"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestWildcardPlugin(t *testing.T) { //This is what is run when 'go test' or ginko 
+	//When ginkgo test fails, ginkgo's Fail(description string) function gets called
+	//This function is passed to Gomega using RegisterFailHandler, the sole connection point b/w Ginkgo and Gomega.
+	RegisterFailHandler(Fail)
+	//RunSpace(t *testing.T, suiteDescription string) tells Ginkgo to start the test suite.
+	//When any spec fails, testing.T is automatically failed by Ginkgo
+	RunSpecs(t, "WildcardPlugin Suite")
+}
