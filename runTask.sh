e0a1bb830fa205efd41b60834048eb1ffde7ce31
Jeanie <ejung@pivotal.io>
2015-06-29 09:28:50 -0700

    runTask.sh updated

--- runTask.sh
+++ runTask.sh
@@ -1,2 +1,3 @@
 fly execute -c ./ci/scripts/build/task.yml --i repo=./ --privileged
-fly execute -c ./ci/scripts/test/task.yml --i repo=./ --privileged --i binaries=./binaries
+fly execute -c ./ci/scripts/test/task.yml --i repo=./ --privileged --i binaries=./binaries cf_password=____ binary-name=wildcard binary-version=0.0.9 binary-bucket="cf-wildcard-plugin" cf_api_endpoint=api.run.pivotal.io cf_user=ejung@pivotal.io cf_org=platform-eng cf_space=Jeanie cf_password=____ binary-name=wildcard binary-version=0.0.9 binary-bucket="cf-wildcard-plugin" cf_api_endpoint=api.run.pivotal.io cf_user=ejung@pivotal.io cf_org=platform-eng cf_space=Jeanie 
+
